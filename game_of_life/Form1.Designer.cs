dc732aef4ce04d9350f7768cb20cf464bafb6b59
Moroes <62904051+Moroes@users.noreply.github.com>
2021-08-02 14:24:31 +0300

    Initial commit

---
+++ game_of_life/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace game_of_life
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnReadField = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.cbRandom = new System.Windows.Forms.CheckBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.cbColor = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.nudDensity = new System.Windows.Forms.NumericUpDown();
+            this.lblDensity = new System.Windows.Forms.Label();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDensity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.splitContainer1.Panel1.Controls.Add(this.btnReadField);
+            this.splitContainer1.Panel1.Controls.Add(this.btnClear);
+            this.splitContainer1.Panel1.Controls.Add(this.cbRandom);
+            this.splitContainer1.Panel1.Controls.Add(this.btnRead);
+            this.splitContainer1.Panel1.Controls.Add(this.btnSave);
+            this.splitContainer1.Panel1.Controls.Add(this.cbColor);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.btnStop);
+            this.splitContainer1.Panel1.Controls.Add(this.btnStart);
+            this.splitContainer1.Panel1.Controls.Add(this.nudDensity);
+            this.splitContainer1.Panel1.Controls.Add(this.lblDensity);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.rtbLog);
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(839, 532);
+            this.splitContainer1.SplitterDistance = 176;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // btnReadField
+            // 
+            this.btnReadField.Location = new System.Drawing.Point(39, 441);
+            this.btnReadField.Margin = new System.Windows.Forms.Padding(4);
+            this.btnReadField.Name = "btnReadField";
+            this.btnReadField.Size = new System.Drawing.Size(137, 34);
+            this.btnReadField.TabIndex = 10;
+            this.btnReadField.Text = "Считать поле";
+            this.btnReadField.UseVisualStyleBackColor = true;
+            this.btnReadField.Click += new System.EventHandler(this.btnReadField_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(37, 481);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(139, 36);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Очистить поле";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // cbRandom
+            // 
+            this.cbRandom.Location = new System.Drawing.Point(37, 193);
+            this.cbRandom.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cbRandom.Name = "cbRandom";
+            this.cbRandom.Size = new System.Drawing.Size(165, 59);
+            this.cbRandom.TabIndex = 8;
+            this.cbRandom.Text = "Автоматическая генерация начальных клеток";
+            this.cbRandom.UseVisualStyleBackColor = true;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(37, 388);
+            this.btnRead.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(139, 46);
+            this.btnRead.TabIndex = 7;
+            this.btnRead.Text = "Считать данные";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(37, 338);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(139, 46);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Сохранить данные";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // cbColor
+            // 
+            this.cbColor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbColor.Items.AddRange(new object[] {
+            "Красный",
+            "Синий",
+            "Зеленый"});
+            this.cbColor.Location = new System.Drawing.Point(37, 156);
+            this.cbColor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cbColor.Name = "cbColor";
+            this.cbColor.Size = new System.Drawing.Size(137, 24);
+            this.cbColor.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Цвет клеток";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStop.Location = new System.Drawing.Point(37, 297);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(139, 34);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Стоп";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.AutoSize = true;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStart.Location = new System.Drawing.Point(37, 257);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(139, 34);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // nudDensity
+            // 
+            this.nudDensity.Location = new System.Drawing.Point(37, 84);
+            this.nudDensity.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.nudDensity.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudDensity.Name = "nudDensity";
+            this.nudDensity.Size = new System.Drawing.Size(133, 22);
+            this.nudDensity.TabIndex = 1;
+            this.nudDensity.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // lblDensity
+            // 
+            this.lblDensity.Location = new System.Drawing.Point(33, 21);
+            this.lblDensity.Name = "lblDensity";
+            this.lblDensity.Size = new System.Drawing.Size(137, 60);
+            this.lblDensity.TabIndex = 0;
+            this.lblDensity.Text = "Вероятность появления клетки 1 к\r\n";
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.rtbLog.Location = new System.Drawing.Point(0, 431);
+            this.rtbLog.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.ReadOnly = true;
+            this.rtbLog.Size = new System.Drawing.Size(657, 99);
+            this.rtbLog.TabIndex = 7;
+            this.rtbLog.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(657, 530);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 532);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nudDensity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown nudDensity;
+        private System.Windows.Forms.Label lblDensity;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox cbColor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbLog;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.CheckBox cbRandom;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnReadField;
+    }
+}
+
---
+++ game_of_life/Form1.cs
@@ -0,0 +1,325 @@
+﻿using PeanutButter.INI;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game_of_life
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        // начальные значения
+        private IINIFile _loadedConfig;
+        private Graphics graphics;
+        private int currentGeneration = 0;
+        private int rows = 100;
+        private int columns = 100;
+        private bool[,] field;
+        int size_rect;
+        private string path = (Directory.GetCurrentDirectory() + "\\Settings\\");
+        Brush[] colors = { Brushes.Red, Brushes.Blue, Brushes.Green }; // цвета клеток
+
+        //основной таймер генерации поколения
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            nextGeneration();
+        }
+
+        //начальный генератор клеток
+        private void random_generation()
+        {
+            field = new bool[columns, rows];
+            Random random = new Random();
+            for (int x = 0; x < columns; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    field[x, y] = random.Next((int)nudDensity.Value) == 0;
+                }
+            }
+        }
+        
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            
+            // проверка на уже запущенную игру
+            if (timer1.Enabled)
+                return;
+
+            currentGeneration = 0;
+            rtbLog.Text = "";
+            if (cbRandom.Checked)
+                random_generation();
+            timer1.Start();
+        }
+
+        //функция генерации клеток
+        private void nextGeneration()
+        {
+            graphics.Clear(Color.White);
+
+            var newField = new bool[columns, rows];
+
+            for (int x = 0; x < columns; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    var neighboursCount = countNeighbours(x, y);
+                    var hasLife = field[x, y];
+
+                    if (!hasLife && neighboursCount == 3) // сохранение жизни клетки/создание новой
+                    {
+                        newField[x, y] = true;
+                    }
+                    else if (hasLife && (neighboursCount < 2 || neighboursCount > 3)) // переполнение клеток
+                    {
+                        newField[x, y] = false;
+                    }
+                    else
+                    {
+                        newField[x, y] = field[x, y];
+                    }
+
+                    if (hasLife)
+                    {
+                        // отрисовка прямоугольника
+                        graphics.FillRectangle(get_color(), x * size_rect, y * size_rect, size_rect, size_rect);
+                    }
+                }
+            }
+            // вывод данных в log
+            rtbLog.AppendText(String.Format("Текущее поколение:{0} | Живых клеток:{1} | Цвет клеток:{2}\n", currentGeneration,get_number_of_living(rows,columns),cbColor.SelectedItem));
+            rtbLog.ScrollToCaret();
+            currentGeneration++;
+            this.Text = "Поколение:" + currentGeneration;
+            field = newField;
+            pictureBox1.Refresh();
+        }
+
+        // функция получения количества живых клеток на поле
+        private int get_number_of_living(int rows, int columns)
+        {
+            int count = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (field[j, i])
+                        count++;
+            }
+                }
+            return count;
+        }
+        
+        // функция получения цвета
+        private Brush get_color()
+        {
+            return colors[cbColor.SelectedIndex];
+        }
+
+        //функция подсчета соседних клеток
+        private int countNeighbours(int x, int y)//
+        {
+            int count = 0;
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    int col = (x + i + columns) % columns;
+                    int row = (y + j + rows) % rows;
+
+                    var isSelfChecking = col == x && row == y;
+                    var hasLife = field[col, row];                  
+                    if (hasLife && !isSelfChecking)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        private void stopGame()
+        {
+            if (!timer1.Enabled)
+                return;
+            timer1.Stop();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            stopGame();
+        }
+
+        //создание(удаление) клетки на лкм/пкм
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                var x = e.Location.X / size_rect;
+                var y = e.Location.Y / size_rect;
+                if (validateMousePosition(x, y))
+                {
+                    field[x, y] = true;
+                    graphics.FillRectangle(get_color(), x * size_rect, y * size_rect, size_rect, size_rect);
+                    pictureBox1.Refresh();
+                }
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                var x = e.Location.X / size_rect;
+                var y = e.Location.Y / size_rect;
+                if (validateMousePosition(x, y))
+                {
+                    field[x, y] = false;
+                    graphics.FillRectangle(Brushes.Black, x * size_rect, y * size_rect, size_rect, size_rect);
+                    pictureBox1.Refresh();
+                }
+            }
+        }
+
+
+        //функция проверки положения мыши относительно рабочего окна игры
+        private bool validateMousePosition(int x, int y)
+        {
+            return x >= 0 && y >= 0 && y < columns && x < rows;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbColor.SelectedIndex = 0;
+            size_rect = pictureBox1.Height / rows;
+            pictureBox1.Image = new Bitmap(splitContainer1.Panel2.Width, splitContainer1.Panel2.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+            graphics.Clear(Color.White);
+            field = new bool[rows, columns];
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                clear_field();
+                _loadedConfig = new INIFile(Directory.GetCurrentDirectory() + "\\Settings\\settings.ini");
+                nudDensity.Value = _loadedConfig.HasSetting("UserSettings", "density") ? int.Parse(_loadedConfig["UserSettings"]["density"]) : 2;
+                cbColor.SelectedIndex = _loadedConfig.HasSetting("UserSettings", "color_index") ? int.Parse(_loadedConfig["UserSettings"]["color_index"]) : -1;
+                read_field("field.txt");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            _loadedConfig = new INIFile(path + "settings.ini");
+            _loadedConfig["UserSettings"]["density"] = nudDensity.Value.ToString();
+            _loadedConfig["UserSettings"]["color_index"] = cbColor.SelectedIndex.ToString();
+
+            _loadedConfig.Persist();
+            save_field();
+        }
+
+        private void save_field()
+        {
+            using (StreamWriter sw = new StreamWriter(path + "field.txt", false, System.Text.Encoding.Default))
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        sw.Write(field[i,j] ? 1 : 0);
+                    }
+                    sw.WriteLine();
+                }
+            }
+        }
+
+        private void read_field(string name_field)
+        {
+            using (StreamReader sr = new StreamReader(path + name_field, System.Text.Encoding.Default))
+            {
+                string line;
+                for (int i = 0; i < rows; i++)
+                {
+                    line = sr.ReadLine(); // считывание строки
+                    for (int j = 0; j < columns; j++)
+                    {
+                        try
+                        {
+                            field[i, j] = line[j] == '1' ? true : false; // 
+                            if (field[i, j])
+                                graphics.FillRectangle(get_color(), i * size_rect, j * size_rect, size_rect, size_rect);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                            return;
+                        }
+                    }
+                }
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clear_field();
+        }
+
+        private void clear_field()
+        {
+            field = new bool[rows, columns];
+            graphics.Clear(Color.White);
+            pictureBox1.Refresh();
+        }
+
+        // масштабирование поля
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            size_rect = pictureBox1.Height / rows;
+            pictureBox1.Image = new Bitmap(splitContainer1.Panel2.Width, splitContainer1.Panel2.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+            graphics.Clear(Color.White);
+        }
+
+        // считывание поля из файла
+        private void btnReadField_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = path;
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    clear_field();
+                    string name_field = Path.GetFileName(openFileDialog.FileName);
+                    read_field(name_field);
+                }
+            }
+        }        
+    }
+}
